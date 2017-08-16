59056c6638eae8ed67e9b49a016fb3ae0524a5e0
Mike San Román <mike@msanroman.io>
2017-08-16 18:54:50 +0200

    Reducer updates the profiles state once a a new result comes from async-data-fetch

--- packages/nav-sidebar/reducer.test.js
+++ packages/nav-sidebar/reducer.test.js
@@ -1,8 +1,20 @@
+import { actionTypes } from '@bufferapp/async-data-fetch';
 import reducer from './reducer';
 
 describe('reducer', () => {
   it('has no profiles as initial state', () => {
-    const profiles = reducer().profiles;
+    const profiles = reducer(undefined, {
+      type: 'TEST_ACTION',
+    }).profiles;
     expect(profiles).toEqual([]);
+  });
+
+  it('updates the state with the profiles received on fetch_success', () => {
+    const profiles = ['a profile', 'another profile', 'a third profile'];
+    const state = reducer(undefined, {
+      type: `profiles_${actionTypes.FETCH_SUCCESS}`,
+      result: profiles,
+    });
+    expect(state.profiles).toEqual(profiles);
   });
 });
