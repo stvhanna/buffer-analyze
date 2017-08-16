809efcfa961d4b22607958afd92d7e5d6939c3a8
Mike San Román <mike@msanroman.io>
2017-08-16 18:30:11 +0200

    Ensure we keep propagating the intercepted action.

--- packages/nav-sidebar/middleware.test.js
+++ packages/nav-sidebar/middleware.test.js
@@ -1,7 +1,28 @@
 import middleware from './middleware';
 
+const getMiddlewareElements = () => {
+  const store = {
+    getState: jest.fn(() => ({})),
+    dispatch: jest.fn(),
+  };
+  const next = jest.fn();
+
+  const invoke = action => middleware(store)(next)(action);
+
+  return { store, next, invoke };
+};
+
 describe('middleware', () => {
   it('should exist', () => {
     expect(middleware).toBeDefined();
+  });
+
+  it('should keep propagating the action through the chain', () => {
+    const { next, invoke } = getMiddlewareElements();
+    const action = {
+      type: 'TEST',
+    };
+    invoke(action);
+    expect(next).toHaveBeenCalledWith(action);
   });
 });
