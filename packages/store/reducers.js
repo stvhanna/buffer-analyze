dcef5a25854cd76efd974cbe697bc4bbd9ba0c15
Mike San Román <mike@msanroman.io>
2017-08-17 14:11:12 +0200

    Get rid of the example package

--- packages/store/reducers.js
+++ packages/store/reducers.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as loginReducer } from '@bufferapp/login';
-import { reducer as exampleReducer } from '@bufferapp/example';
 import { reducer as i18nReducer } from '@bufferapp/analyze-i18n';
 import { reducer as appSidebarReducer } from '@bufferapp/app-sidebar';
 import { reducer as asyncDataFetchReducer } from '@bufferapp/async-data-fetch';
@@ -10,7 +9,6 @@ import { reducer as navSidebarReducer } from '@bufferapp/nav-sidebar';
 export default combineReducers({
   login: loginReducer,
   router: routerReducer,
-  example: exampleReducer,
   appSidebar: appSidebarReducer,
   asyncDataFetch: asyncDataFetchReducer,
   i18n: i18nReducer,
