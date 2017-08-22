c10e49cdd489e68f7b0572e2ccbed9ed1765731c
Federico Weber <federicoweber@gmail.com>
2017-08-21 17:02:25 -0700

    add Comparisons page placeholder

--- packages/web/components/App/index.jsx
+++ packages/web/components/App/index.jsx
@@ -4,6 +4,7 @@ import AppSidebar from '@bufferapp/app-sidebar';
 import DefaultPage from '../DefaultPage';
 import ReportsPage from '../ReportsPage';
 import InsightsPage from '../InsightsPage';
+import ComparisonsPage from '../ComparisonsPage';
 
 const appStyle = {
   display: 'flex',
@@ -26,6 +27,10 @@ export default () =>
         <Route
           path="/insights/*"
           component={InsightsPage}
+        />
+        <Route
+          path="/comparisons"
+          component={ComparisonsPage}
         />
         <Route component={DefaultPage} />
       </Switch>
