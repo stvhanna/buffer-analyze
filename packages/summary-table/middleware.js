715ac53fd7143710681f77e28a941c3ff9d6fff0
Mike San Román <mike@msanroman.io>
2017-08-23 15:45:19 +0200

    Implement middleware

---
+++ packages/summary-table/middleware.js
@@ -0,0 +1,18 @@
+import { actions } from '@bufferapp/async-data-fetch';
+
+export default ({ dispatch }) => next => (action) => { // eslint-disable-line no-unused-vars
+  next(action);
+  switch (action.type) {
+    case 'PROFILE_SELECTED':
+      dispatch(actions.fetch({
+        name: 'summary',
+        args: {
+          profileId: action.id,
+        },
+      }));
+      break;
+    default:
+      break;
+  }
+  return next(action);
+};
---
+++ packages/summary-table/middleware.test.js
@@ -0,0 +1,34 @@
+import { actions } from '@bufferapp/async-data-fetch';
+import middleware from './middleware';
+
+describe('middleware', () => {
+  const next = jest.fn();
+  const store = {
+    dispatch: jest.fn(),
+  };
+  it('should exist', () => {
+    expect(middleware).toBeDefined();
+  });
+
+  it('should keep propagating the action through the chain', () => {
+    const action = {
+      type: 'TEST',
+    };
+    middleware(store)(next)(action);
+  });
+
+  it('shoud dispatch a data fetch for summary once a profile has been selected', () => {
+    const action = {
+      type: 'PROFILE_SELECTED',
+      id: '1235519asd',
+    };
+    middleware(store)(next)(action);
+    expect(store.dispatch).toHaveBeenCalledWith(actions.fetch({
+      name: 'summary',
+      args: {
+        profileId: '1235519asd',
+      },
+    }));
+    expect(next).toHaveBeenCalledWith(action);
+  });
+});
