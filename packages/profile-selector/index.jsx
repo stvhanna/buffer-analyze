39451c49967adcd7739208e70b4dba5c3ecae721
Federico Weber <federicoweber@gmail.com>
2017-08-24 15:00:46 -0700

    working on container component

--- packages/profile-selector/index.jsx
+++ packages/profile-selector/index.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const ProfileSelector = () => (
-  (<div> yeah! </div>)
+import ProfileSelectorDropdown from './components/ProfileSelectorDropdown';
+import { actions } from './reducer';
+
+const ProfileSelector = props => (
+  <ProfileSelectorDropdown {...props} />
 );
 
+ProfileSelector.propTypes = {
+  profiles: PropTypes.arrayOf(PropTypes.shape({
+    service: PropTypes.string.isRequired,
+    selected: PropTypes.bool.isRequired,
+    handle: PropTypes.string.isRequired,
+    avatarUrl: PropTypes.string.isRequired,
+  })).isRequired,
+  isDropdownOpen: PropTypes.bool.isRequired,
+  selectProfile: PropTypes.func.isRequired,
+  toggleDropdown: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state, props) => ({
+  isDropdownOpen: false,
+  profiles: state.navSidebar.profiles.filter(p => p.service === props.location.match.service),
+});
+
+const mapDispatchToProps = dispatch => ({
+  selectProfile: id => dispatch(actions.selectedProfile({ id })),
+  toggleDropdown: () => dispatch(actions.toggleDropdown),
+});
+
 export default connect(
-  state => ({
-    profiles: state.navSidebar.profiles,
-  }),
+  mapStateToProps,
+  mapDispatchToProps,
 )(ProfileSelector);
 
 export reducer, { actionTypes, actions } from './reducer';
