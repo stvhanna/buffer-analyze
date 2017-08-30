9a8c9c3e5cca8c37e54d75e3e7afc6cf5f3f8e96
Mike San Román <mike@msanroman.io>
2017-08-30 19:34:13 +0200

    Fix middleware test for summary

--- packages/summary-table/middleware.test.js
+++ packages/summary-table/middleware.test.js
@@ -2,10 +2,25 @@ import { actions } from '@bufferapp/async-data-fetch';
 import { actionTypes } from '@bufferapp/analyze-profile-selector';
 import middleware from './middleware';
 
+const profileId = '12359182129asd';
+
+const state = {
+  router: {
+    location: {
+      pathname: `/insights/twitter/${profileId}`,
+    },
+  },
+  date: {
+    startDate: '10/10/2016',
+    endDate: '30/10/2016',
+  },
+};
+
 describe('middleware', () => {
   const next = jest.fn();
   const store = {
     dispatch: jest.fn(),
+    getState: jest.fn(() => state),
   };
   it('should exist', () => {
     expect(middleware).toBeDefined();
@@ -28,6 +43,8 @@ describe('middleware', () => {
       name: 'summary',
       args: {
         profileId: '1235519asd',
+        startDate: state.date.startDate,
+        endDate: state.date.endDate,
       },
     }));
     expect(next).toHaveBeenCalledWith(action);
