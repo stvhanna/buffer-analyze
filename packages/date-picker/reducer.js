226199b3f94bb8e926f6f2cbd15ceaf6a5c1628d
Mike San Román <mike@msanroman.io>
2017-08-30 12:12:13 +0200

    Add initial implementation for the reducer.

--- packages/date-picker/reducer.js
+++ packages/date-picker/reducer.js
@@ -1,12 +1,65 @@
+import moment from 'moment';
+
 import { actionTypes as asyncDataFetchActionTypes } from '@bufferapp/async-data-fetch';
 
-export const actionTypes = {};
+export const actionTypes = {
+  OPEN_DATE_PICKER: 'OPEN_DATE_PICKER',
+  CLOSE_DATE_PICKER: 'CLOSE_DATE_PICKER',
+  SET_MIN_DATE: 'SET_MIN_DATE',
+  SET_DATE_RANGE: 'SET_DATE_RANGE',
+};
 
 const initialState = {
+  loading: true,
+  startDate: moment().subtract('7', 'days').unix(),
+  endDate: moment().subtract('1', 'days').unix(),
+  open: false,
+  minDate: null,
 };
 
 export default (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case actionTypes.SET_DATE_RANGE:
+      return {
+        ...state,
+        startDate: action.startDate,
+        endDate: action.endDate,
+      };
+    case actionTypes.SET_MIN_DATE:
+      return {
+        ...state,
+        loading: false,
+        minDate: action.date,
+      };
+    case actionTypes.OPEN_DATE_PICKER:
+      return {
+        ...state,
+        open: true,
+      };
+    case actionTypes.CLOSE_DATE_PICKER:
+      return {
+        ...state,
+        open: false,
+      };
+    default:
+      return state;
+  }
 };
 
-export const actions = {};
+export const actions = {
+  open: () => ({
+    type: actionTypes.OPEN_DATE_PICKER,
+  }),
+  close: () => ({
+    type: actionTypes.CLOSE_DATE_PICKER,
+  }),
+  setMinDate: date => ({
+    type: actionTypes.SET_MIN_DATE,
+    date,
+  }),
+  setDateRange: (startDate, endDate) => ({
+    type: actionTypes.SET_DATE_RANGE,
+    startDate,
+    endDate,
+  }),
+};
--- packages/date-picker/reducer.test.js
+++ packages/date-picker/reducer.test.js
@@ -1,9 +1,113 @@
-import deepFreeze from 'deep-freeze';
+import moment from 'moment';
 import { actionTypes as asyncDataFetchActions } from '@bufferapp/async-data-fetch';
-import reducer from './reducer';
+import reducer, { actions, actionTypes } from './reducer';
 
 describe('reducer', () => {
-  it ('should work', () => {
-    fail();
+  describe('initial state', () => {
+    let state;
+
+    beforeEach(() => {
+      state = reducer(undefined, {
+        type: 'TEST_ACTION',
+      });
+    });
+
+    it('is loading', () => {
+      expect(state.loading).toBeTruthy();
+    });
+
+    it('has the past seven days date range selected', () => {
+      const yesterday = moment().subtract(1, 'day').format('MM/DD/YYYY');
+      const sevenDaysAgo = moment().subtract(7, 'day').format('MM/DD/YYYY');
+      expect(moment.unix(state.startDate).format('MM/DD/YYYY')).toBe(sevenDaysAgo);
+      expect(moment.unix(state.endDate).format('MM/DD/YYYY')).toBe(yesterday);
+    });
+
+    it('is closed', () => {
+      expect(state.open).toBe(false);
+    });
+
+    it('has no minimum date set', () => {
+      expect(state.minDate).toBe(null);
+    });
+  });
+
+  describe('opening and closing the date picker', () => {
+    it(`should open when receiving ${actionTypes.OPEN_DATE_PICKER}`, () => {
+      const state = reducer(undefined, {
+        type: actionTypes.OPEN_DATE_PICKER,
+      });
+      expect(state.open).toBeTruthy();
+    });
+
+    it(`should close when receiving ${actionTypes.CLOSE_DATE_PICKER}`, () => {
+      const state = reducer(undefined, {
+        type: actionTypes.CLOSE_DATE_PICKER,
+      });
+      expect(state.open).toBe(false);
+    });
+  });
+
+  describe(`${actionTypes.SET_MIN_DATE}`, () => {
+    it(`stops loading state when receiving ${actionTypes.SET_MIN_DATE}`, () => {
+      const state = reducer(undefined, {
+        type: actionTypes.SET_MIN_DATE,
+      });
+      expect(state.loading).toBe(false);
+    });
+
+    it('sets the minimum date available', () => {
+      const date = moment().subtract(34, 'days').unix();
+      const state = reducer(undefined, {
+        type: actionTypes.SET_MIN_DATE,
+        date,
+      });
+      expect(state.minDate).toBe(date);
+    });
+  });
+
+  it(`changes the date range when receiving ${actionTypes.SET_DATE_RANGE}`, () => {
+    const startDate = moment().subtract(4, 'days').unix();
+    const endDate = moment().subtract(2, 'days').unix();
+    const state = reducer(undefined, {
+      type: actionTypes.SET_DATE_RANGE,
+      startDate,
+      endDate,
+    });
+    expect(state.startDate).toBe(startDate);
+    expect(state.endDate).toBe(endDate);
+  });
+});
+
+describe('actions', () => {
+  describe('opening and closing', () => {
+    it(`open triggers ${actionTypes.OPEN_DATE_PICKER}`, () => {
+      expect(actions.open()).toEqual({
+        type: actionTypes.OPEN_DATE_PICKER,
+      });
+    });
+    it(`close triggers ${actionTypes.CLOSE_DATE_PICKER}`, () => {
+      expect(actions.close()).toEqual({
+        type: actionTypes.CLOSE_DATE_PICKER,
+      });
+    });
+  });
+
+  it(`setMinDate triggers ${actionTypes.SET_MIN_DATE}`, () => {
+    const minDate = moment().subtract(64, 'days').unix();
+    expect(actions.setMinDate(minDate)).toEqual({
+      type: actionTypes.SET_MIN_DATE,
+      date: minDate,
+    });
+  });
+
+  it(`setDateRange triggers ${actionTypes.SET_DATE_RANGE}`, () => {
+    const start = moment().subtract(30, 'days').unix();
+    const end = moment().subtract(1, 'days').unix();
+    expect(actions.setDateRange(start, end)).toEqual({
+      type: actionTypes.SET_DATE_RANGE,
+      startDate: start,
+      endDate: end,
+    });
   });
 });
