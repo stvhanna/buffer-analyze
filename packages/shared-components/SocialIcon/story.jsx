e5c79306725066b00163a1c5e8df8ae689b3fde0
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-08-31 19:09:28 +0400

    Create stories for shared-components

--- packages/shared-components/SocialIcon/story.jsx
+++ packages/shared-components/SocialIcon/story.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from 'storybook-addon-a11y';
-import moment from 'moment';
-import PeriodPhrase from './index';
+import SocialIcon from './index';
 
-storiesOf('PeriodPhrase')
+storiesOf('SocialIcon')
   .addDecorator(checkA11y)
   .add('should render', () => (
-    <PeriodPhrase startDate={moment().subtract(7, 'days').unix()} endDate={moment().subtract(1, 'day').unix()} />
+    <SocialIcon
+      service={'facebook'}
+      socialIconSize={16}
+      avatarSize={32}
+    />
   ));
 
