35ae1245b4f1b3eb10a3bbda9ed0d54daaef8242
Federico Weber <federicoweber@gmail.com>
2017-08-31 18:14:07 -0700

    add in middleware and reducer

--- packages/average-table/middleware.js
+++ packages/average-table/middleware.js
@@ -1,8 +1,45 @@
+import { actions } from '@bufferapp/async-data-fetch';
+import { actionTypes as profileActionTypes } from '@bufferapp/analyze-profile-selector';
+import { actionTypes as dateActionTypes } from '@bufferapp/analyze-date-picker';
+
+const getProfileIdFromRoute = (state) => {
+  const currentRoute = state.router.location.pathname;
+  const routeMatch = currentRoute.match(/\/insights\/.*\/(.*)/);
+  let profileId;
+  if (routeMatch) {
+    profileId = routeMatch[1];
+  } else {
+    profileId = null;
+  }
+  return profileId;
+};
+
 export default store => next => (action) => { // eslint-disable-line no-unused-vars
-  /* eslint-disable no-console */
-  console.group();
-  console.log('action', action);
-  console.groupEnd();
-  /* eslint-enable no-console */
+  const { dispatch, getState } = store;
+  switch (action.type) {
+    case profileActionTypes.SELECT_PROFILE:
+      dispatch(actions.fetch({
+        name: 'average',
+        args: {
+          profileId: action.id,
+          startDate: getState().date.startDate,
+          endDate: getState().date.endDate,
+        },
+      }));
+      break;
+    case dateActionTypes.SET_DATE_RANGE:
+      dispatch(actions.fetch({
+        name: 'average',
+        args: {
+          profileId: getProfileIdFromRoute(getState()),
+          startDate: action.startDate,
+          endDate: action.endDate,
+        },
+      }));
+      break;
+    default:
+      break;
+  }
   return next(action);
 };
+
