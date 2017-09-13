667b5b24aa9f35dbe81773c87c3e57c1286a34ca
Mike San Román <mike@msanroman.io>
2017-09-13 16:16:21 +0200

    Fix scroll
    Keep sidebar and tabs fixed and just allow the tab content to scroll
    when it overflows the window height.

--- packages/profile-loader/components/ProfileLoader/index.jsx
+++ packages/profile-loader/components/ProfileLoader/index.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Loader from '@bufferapp/components/Loader';
 
+const pageContentStyle = {
+  padding: '0rem 2rem',
+  display: 'flex',
+  flexGrow: 1,
+  flexDirection: 'column',
+  height: '100vh',
+};
+
 const ProfileLoader = ({ loading, children }) => {
   if (loading) {
     return (<Loader>Loading...</Loader>);
   }
-  return (<div>{children}</div>);
+  return (<div style={pageContentStyle}>{children}</div>);
+};
+
+ProfileLoader.defaultProps = {
+  loading: false,
+};
+
+ProfileLoader.propTypes = {
+  loading: PropTypes.bool,
+  children: PropTypes.arrayOf(PropTypes.node).isRequired,
 };
 
 export default ProfileLoader;
