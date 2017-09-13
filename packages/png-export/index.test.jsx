e56742cd5ade0cf184ead1b0a7ea495e401ce257
Mike San Román <mike@msanroman.io>
2017-09-13 09:19:34 +0200

    Add presentational component

---
+++ packages/png-export/index.test.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import PngExport, {
+  reducer,
+  actions,
+  actionTypes,
+  middleware,
+} from './index';
+import ExportButton from './components/ExportButton';
+
+const storeFake = state => ({
+  default: () => {},
+  subscribe: () => {},
+  dispatch: () => {},
+  getState: () => ({ ...state }),
+});
+
+describe('PngExport', () => {
+  it('should render', () => {
+    const store = storeFake({
+    });
+    const wrapper = mount(
+      <Provider store={store}>
+        <PngExport />
+      </Provider>,
+    );
+    expect(wrapper.find(ExportButton).length)
+      .toBe(1);
+  });
+
+  it('should export reducer', () => {
+    expect(reducer)
+      .toBeDefined();
+  });
+
+  it('should export actions', () => {
+    expect(actions)
+      .toBeDefined();
+  });
+
+  it('should export actionTypes', () => {
+    expect(actionTypes)
+      .toBeDefined();
+  });
+
+  it('should export middleware', () => {
+    expect(middleware)
+      .toBeDefined();
+  });
+});
