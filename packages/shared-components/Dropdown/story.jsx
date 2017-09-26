c70bcf87419e8feeb25f9eaa64cf59a614d88ad7
Mike San Román <mike@msanroman.io>
2017-09-26 12:03:51 -0400

    Add stories for shared dropdown

---
+++ packages/shared-components/Dropdown/story.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { checkA11y } from 'storybook-addon-a11y';
+import { action } from '@storybook/addon-actions';
+import Dropdown, { DropdownContent } from './index';
+
+storiesOf('Dropdown')
+  .addDecorator(checkA11y)
+  .add('closed dropdown', () => (
+    <div>
+      <Dropdown
+        toggleDropdown={action('toggle')}
+      >This is a dropdown
+        <DropdownContent>
+          <ul>
+            <li>An item</li>
+            <li>Another item</li>
+          </ul>
+        </DropdownContent>
+      </Dropdown>
+    </div>
+  ))
+  .add('open dropdown', () => (
+    <div>
+      <Dropdown
+        toggleDropdown={action('toggle')}
+        isDropdownOpen
+      >This is a dropdown
+        <DropdownContent isDropdownOpen>
+          <ul>
+            <li>An item</li>
+            <li>Another item</li>
+          </ul>
+        </DropdownContent>
+      </Dropdown>
+    </div>
+  ));
+
