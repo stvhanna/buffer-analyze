7ed9a68a105b0de0c3f23368d278b5057884ffee
Federico Weber <federicoweber@gmail.com>
2017-09-27 13:07:28 -0700

    simplify timezone offset for charts

--- packages/shared-components/GridItem/components/ChartTooltip/index.jsx
+++ packages/shared-components/GridItem/components/ChartTooltip/index.jsx
@@ -12,12 +12,12 @@ function transformLabelForTooltip(label) {
 const ChartTooltip = ({ point }) => (
   point.label ?
     (<span style={{ marginLeft: '7px' }}>
-      <Text size="small" >{moment.tz(point.tooltipTime, point.timezone).startOf('day').format('D MMMM')},</Text>
+      <Text size="small" >{moment.tz(point.x, point.timezone).format('D MMMM')},</Text>
       <Text size="small" weight="bold" color="black" > <TruncatedNumber>{point.y}</TruncatedNumber></Text>
       <Text size="small" > {transformLabelForTooltip(point.label)}</Text>
     </span>) :
     (<span style={{ marginLeft: '7px' }}>
-      <Text size="small">No data for {moment.tz(point.x, point.timezone).startOf('day').format('D MMMM')}</Text>
+      <Text size="small">No data for {moment.tz(point.x, point.timezone).format('D MMMM')}</Text>
     </span>)
 );
 
@@ -25,14 +25,8 @@ ChartTooltip.propTypes = {
   point: PropTypes.shape({
     x: PropTypes.number.isRequired,
     y: PropTypes.number.isRequired,
-    tooltipTime: PropTypes.isRequired,
     label: PropTypes.string.isRequired,
-    timezone: PropTypes.string,
   }).isRequired,
-};
-
-ChartTooltip.defaultProps = {
-  timezone: 'Etc/UTC',
 };
 
 export default ChartTooltip;
