1723171a0c3c3a52a0bb1e31037dfc0eba7bfb6d
Mike San Román <mike@msanroman.io>
2017-10-17 16:35:50 +0200

    Top posts table exports to csv

--- packages/top-posts-table/middleware.js
+++ packages/top-posts-table/middleware.js
@@ -1,14 +1,68 @@
+import moment from 'moment';
+import striptags from 'striptags';
 import { actions } from '@bufferapp/async-data-fetch';
 import { actionTypes as profileActionTypes } from '@bufferapp/analyze-profile-selector';
 import { actionTypes as dateActionTypes } from '@bufferapp/analyze-date-picker';
 import { actionTypes as exportActionTypes, actions as exportActions } from '@bufferapp/analyze-png-export';
+import { actionTypes as exportCSVActionTypes, actions as exportCSVActions } from '@bufferapp/analyze-csv-export';
 import { actionTypes as topPostsActionTypes } from './reducer';
+
+const EXPORT_FILENAME = 'top-posts';
+
+const formatMedia = (post) => {
+  if (post.media) {
+    return post.media.video ? post.media.video.details.transcoded_location : post.media.picture;
+  }
+};
+
+const formatDataForKey = (key, post) => {
+  switch (key) {
+    case 'date':
+      return moment(post[key]).format('MM/DD/YYYY HH:mm:ss');
+    case 'media':
+      return formatMedia(post);
+    case 'text':
+      return striptags(post.text, [], '\n').replace(/\n+/gm, ' ');
+    default:
+      return post[key];
+  }
+};
+const formatDataForCSVExport = ({ topPosts }) => {
+  const keys = Object.keys(topPosts[0]);
+  const data = {};
+  keys.forEach((key) => {
+    data[key] = [];
+    topPosts.forEach((post) => {
+      const formattedKey = formatDataForKey(key, post);
+      if (key === 'statistics') {
+        delete data.statistics;
+        Object.keys(formattedKey).forEach((statistic) => {
+          const value = formattedKey[statistic];
+          if (data[statistic]) {
+            data[statistic].push(value);
+          } else data[statistic] = [value];
+        });
+      } else data[key].push(formattedKey);
+    });
+  });
+  return data;
+};
 
 export default store => next => (action) => { // eslint-disable-line no-unused-vars
   const { dispatch, getState } = store;
   switch (action.type) {
+    case exportCSVActionTypes.EXPORT_TO_CSV_START:
+      if (getState().topPosts.topPosts.length) {
+        dispatch(
+          exportCSVActions.exportChart(
+            EXPORT_FILENAME,
+            formatDataForCSVExport(getState().topPosts),
+          ),
+        );
+      }
+      break;
     case exportActionTypes.EXPORT_TO_PNG_START:
-      dispatch(exportActions.exportChart('js-dom-to-png-top-posts', 'top-posts'));
+      dispatch(exportActions.exportChart('js-dom-to-png-top-posts', EXPORT_FILENAME));
       break;
     case topPostsActionTypes.SELECT_TOP_POSTS_METRIC:
       dispatch(actions.fetch({
