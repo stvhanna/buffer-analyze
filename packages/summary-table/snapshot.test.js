350b5551fcbde42f051d9974dd82ef39b1424057
Mike San Román <mike@msanroman.io>
2017-10-18 19:01:30 +0200

    Cleanup snapshot config file

--- packages/summary-table/snapshot.test.js
+++ packages/summary-table/snapshot.test.js
@@ -1,14 +1,8 @@
 // use story.js files as snapshots
 import initStoryshots from '@storybook/addon-storyshots';
-// import shallow from 'react-test-renderer/shallow';
 
 jest.mock('@bufferapp/add-report');
 
 initStoryshots({
   suit: 'Snapshots',
-  // test({ story, context }) {
-  //   const shallowRenderer = shallow.createRenderer();
-  //   const result = shallowRenderer.render(story.render(context));
-  //   expect(result).toMatchSnapshot();
-  // },
 });
