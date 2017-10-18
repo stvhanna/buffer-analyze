6112c4b54e77b067fb46a279afa7ea8f9e498c64
Mike San Román <mike@msanroman.io>
2017-10-18 18:58:56 +0200

    Fix coverage, do not do shallow rendering for summary table

--- packages/summary-table/snapshot.test.js
+++ packages/summary-table/snapshot.test.js
@@ -1,12 +1,14 @@
 // use story.js files as snapshots
 import initStoryshots from '@storybook/addon-storyshots';
-import shallow from 'react-test-renderer/shallow';
+// import shallow from 'react-test-renderer/shallow';
+
+jest.mock('@bufferapp/add-report');
 
 initStoryshots({
   suit: 'Snapshots',
-  test({ story, context }) {
-    const shallowRenderer = shallow.createRenderer();
-    const result = shallowRenderer.render(story.render(context));
-    expect(result).toMatchSnapshot();
-  },
+  // test({ story, context }) {
+  //   const shallowRenderer = shallow.createRenderer();
+  //   const result = shallowRenderer.render(story.render(context));
+  //   expect(result).toMatchSnapshot();
+  // },
 });
