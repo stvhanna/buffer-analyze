1371542cb04bbc85ab629581aa85332909db6b27
Mike San Román <mike@msanroman.io>
2017-10-20 12:26:26 +0200

    Add /create_report call from RPC server

--- packages/server/rpc/createReport/index.js
+++ packages/server/rpc/createReport/index.js
@@ -1,7 +1,21 @@
 const { method } = require('@bufferapp/micro-rpc');
+const rp = require('request-promise');
 
 module.exports = method(
   'create_report',
   'create a new report for Analyze',
-  () => Promise.resolve('yay'),
+  ({ profileId, chartId, name }) =>
+    rp({
+      uri: `${process.env.ANALYZE_API_ADDR}/create_report`,
+      method: 'POST',
+      strictSSL: !(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'),
+      body: {
+        name,
+        profile_id: profileId,
+        chart_id: chartId,
+        user_id: '56c20bd3bd3816f63c94c73f',
+      },
+      json: true,
+    }).then(result => result)
+  ,
 );
---
+++ packages/server/rpc/createReport/index.test.js
@@ -0,0 +1,43 @@
+/* eslint-disable import/first */
+
+jest.mock('micro-rpc-client');
+jest.mock('request-promise');
+import rp from 'request-promise';
+import createReport from './';
+
+describe('rpc/create_report', () => {
+  const profileId = '123159ad';
+  const chartId = 'summary-table';
+  const name = 'Weekly Sync Report';
+
+  it('should have the expected name', () => {
+    expect(createReport.name)
+      .toBe('create_report');
+  });
+
+  it('should have the expected docs', () => {
+    expect(createReport.docs)
+      .toBe('create a new report for Analyze');
+  });
+
+  it('should send a POST request to /create_report with the provided parameters', async () => {
+    rp.mockReturnValueOnce(Promise.resolve('success'));
+
+    const result = await createReport.fn({ profileId, chartId, name });
+
+    expect(result).toBe('success');
+    expect(rp.mock.calls[0]).toEqual([{
+      uri: `${process.env.ANALYZE_API_ADDR}/create_report`,
+      method: 'POST',
+      strictSSL: !(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'),
+      body: {
+        name,
+        profile_id: profileId,
+        chart_id: chartId,
+        user_id: '56c20bd3bd3816f63c94c73f',
+      },
+      json: true,
+    }]);
+  });
+});
+
