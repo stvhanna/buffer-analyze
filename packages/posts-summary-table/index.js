1f5c8e80f3f6bc499abb64650a5d8f3ee95216ac
Mike San Román <mike@msanroman.io>
2017-11-06 20:08:05 +0100

    Add "Add to Report" button to posts-summary-table

--- packages/posts-summary-table/index.js
+++ packages/posts-summary-table/index.js
@@ -14,3 +14,5 @@ export default connect(
 // export reducer, actions and action types
 export reducer, { actions, actionTypes } from './reducer';
 export middleware from './middleware';
+export { Table } from './components/PostsSummaryTable';
+export Title from './components/Title';
