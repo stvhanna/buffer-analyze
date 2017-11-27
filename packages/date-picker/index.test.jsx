919b13ad88c05aff01c6714724cd404db9f57653
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-11-27 15:17:48 -0500

    Test dispatches in the date picker package

--- packages/date-picker/index.test.jsx
+++ packages/date-picker/index.test.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { configure, mount } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 import DatePickerContainer, {
   reducer,
   actions,
@@ -19,19 +20,27 @@ const storeFake = state => ({
 });
 
 describe('DatePicker', () => {
+  const state = {
+    date: {
+      loading: true,
+      startDate: 1511199847,
+      endDate: 1511718247,
+      open: false,
+      calendarOpen: false,
+      minDate: 1463437140000,
+      maxDate: 1511803062968,
+      month: 1509508800,
+    },
+  };
+
   it('should render', () => {
-    const store = storeFake({
-      date: {
-        loading: true,
-      },
-    });
+    const store = storeFake(state);
     const wrapper = mount(
       <Provider store={store}>
         <DatePickerContainer />
       </Provider>,
     );
-    expect(wrapper.find(DatePicker).length)
-      .toBe(1);
+    expect(wrapper.find(DatePicker).length).toBe(1);
   });
 
   it('should export reducer', () => {
@@ -52,5 +61,91 @@ describe('DatePicker', () => {
   it('should export middleware', () => {
     expect(middleware)
       .toBeDefined();
+  });
+
+  it('should dispatch open', () => {
+    const mockStore = configureMockStore();
+    const store = mockStore(state);
+    const component = shallow(<DatePickerContainer store={store} />);
+    expect(component
+        .props()
+        .open()).toEqual(actions.open());
+  });
+
+  it('should dispatch close', () => {
+    const mockStore = configureMockStore();
+    const store = mockStore(state);
+    const component = shallow(<DatePickerContainer store={store} />);
+    expect(component
+        .props()
+        .close()).toEqual(actions.close());
+  });
+
+  it('should dispatch clearStartDate', () => {
+    const mockStore = configureMockStore();
+    const store = mockStore(state);
+    const component = shallow(<DatePickerContainer store={store} />);
+    expect(component
+        .props()
+        .clearStartDate()).toEqual(actions.clearStartDate());
+  });
+
+  it('should dispatch setDateRange', () => {
+    const mockStore = configureMockStore();
+    const store = mockStore(state);
+    const component = shallow(<DatePickerContainer store={store} />);
+    expect(component
+        .props()
+        .setDateRange(111, 222)).toEqual(actions.setDateRange(111, 222));
+  });
+
+  it('should dispatch setMonth', () => {
+    const mockStore = configureMockStore();
+    const store = mockStore(state);
+    const component = shallow(<DatePickerContainer store={store} />);
+    expect(component.props().setMonth(12)).toEqual(actions.setMonth(12));
+  });
+
+  it('should dispatch setStartDate', () => {
+    const mockStore = configureMockStore();
+    const store = mockStore(state);
+    const component = shallow(<DatePickerContainer store={store} />);
+    expect(component
+        .props()
+        .setStartDate(123)).toEqual(actions.setStartDate(123));
+  });
+
+  it('should dispatch setEndDate', () => {
+    const mockStore = configureMockStore();
+    const store = mockStore(state);
+    const component = shallow(<DatePickerContainer store={store} />);
+    expect(component
+        .props()
+        .setEndDate()).toEqual(actions.setEndDate());
+  });
+
+  it('should dispatch clearEndDate', () => {
+    const mockStore = configureMockStore();
+    const store = mockStore(state);
+    const component = shallow(<DatePickerContainer store={store} />);
+    expect(component
+        .props()
+        .clearEndDate()).toEqual(actions.clearEndDate());
+  });
+
+  it('should dispatch selectPreset when value is defined', () => {
+    const mockStore = configureMockStore();
+    const store = mockStore(state);
+    const component = shallow(<DatePickerContainer store={store} />);
+    expect(component
+        .props()
+        .selectPreset(1)).toEqual(undefined);
+  });
+
+  it('should dispatch selectPreset when value is Infinity', () => {
+    const mockStore = configureMockStore();
+    const store = mockStore(state);
+    const component = shallow(<DatePickerContainer store={store} />);
+    expect(component.props().selectPreset(Infinity)).toEqual(undefined);
   });
 });
