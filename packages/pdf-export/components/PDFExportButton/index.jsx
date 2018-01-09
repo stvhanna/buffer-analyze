204038d92091e30fd3516e5f719da3b5a0e1457c
Mike San Román <mike@msanroman.io>
2018-01-09 14:10:35 +0100

    Improve header: add margin between export button and date picker, add correct alignment between those and "Back to reports"

--- packages/pdf-export/components/PDFExportButton/index.jsx
+++ packages/pdf-export/components/PDFExportButton/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Button } from '@bufferapp/analyze-shared-components';
+import Text from '@bufferapp/components/Text';
+
+const Margin = styled.span`
+  margin-left: .5rem;
+`;
 
 const PDFExportButton = ({ exportToPDF }) =>
-  <Button onClick={() => exportToPDF()}>Export as PDF</Button>;
+  <Margin>
+    <Button onClick={() => exportToPDF()}><Text size="small" weight="bold">Export as PDF</Text></Button>
+  </Margin>;
 
 PDFExportButton.propTypes = {
   exportToPDF: PropTypes.func.isRequired,
