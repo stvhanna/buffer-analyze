f7f2d0f9fb1248ab7e56091db8b62577ce6c6a5f
Mike San Román <mike@msanroman.io>
2018-01-11 00:00:44 +0100

    Remove unneded tweaks on initMiddleware - upgrade app-sidebar to 1.2.4

--- packages/store/initMiddleware.js
+++ packages/store/initMiddleware.js
@@ -1,25 +1,12 @@
 import { actions as performanceActions } from '@bufferapp/performance-tracking';
-import {
-  actions as dataFetchActions,
-  actionTypes as dataFetchActionTypes,
-} from '@bufferapp/async-data-fetch';
 
 export default ({ dispatch }) => next => (action) => {
   next(action);
   switch (action.type) {
     case 'APP_INIT':
-        dispatch(performanceActions.measureFromNavigationStart({ name: 'init' }));
-        dispatch(dataFetchActions.fetch({ name: 'user' }));
-        break;
-    case `user_${dataFetchActionTypes.FETCH_SUCCESS}`:
-        /* eslint-disable */
-        if (FS) {
-            FS.identify(action.result.id, {
-              email: action.result.email,
-          });
-        }
-        break;
+      dispatch(performanceActions.measureFromNavigationStart({ name: 'init' }));
+      break;
     default:
-    break;
-}
+      break;
+  }
 };
