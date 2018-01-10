94adaf00d6d64c223465ce3504181523dfc89356
James Morris <me@jamesmorr.is>
2018-01-10 16:43:03 +0000

    Make posts table title adaptable to tweets or posts by profile

--- packages/posts-table/index.jsx
+++ packages/posts-table/index.jsx
@@ -43,7 +43,9 @@ PostsTableWrapper.propTypes = {
 // default export = container
 export default connect(
   (state, props) => ({
-    title: 'Posts',
+    title: (state.profiles.profiles.find(
+      profile => profile.id === props.selectedProfileId,
+    ).service === 'twitter') ? 'Tweet breakdown' : 'Post breakdown',
     loading: state.posts.loading,
     timezone: state.profiles.profiles.find(
       profile => profile.id === props.selectedProfileId,
