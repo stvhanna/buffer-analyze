8125152fe849e9dfb91f2c727c81396a0c4b9640
Tom Redman <tomr@bufferapp.com>
2018-01-10 12:18:01 -0500

    added fullstory

--- packages/store/initMiddleware.js
+++ packages/store/initMiddleware.js
@@ -1,12 +1,25 @@
 import { actions as performanceActions } from '@bufferapp/performance-tracking';
+import {
+  actions as dataFetchActions,
+  actionTypes as dataFetchActionTypes,
+} from '@bufferapp/async-data-fetch';
 
 export default ({ dispatch }) => next => (action) => {
   next(action);
   switch (action.type) {
     case 'APP_INIT':
-      dispatch(performanceActions.measureFromNavigationStart({ name: 'init' }));
-      break;
+        dispatch(performanceActions.measureFromNavigationStart({ name: 'init' }));
+        dispatch(dataFetchActions.fetch({ name: 'user' }));
+        break;
+    case `user_${dataFetchActionTypes.FETCH_SUCCESS}`:
+        /* eslint-disable */
+        if (FS) {
+            FS.identify(action.result.id, {
+              email: action.result.email,
+          });
+        }
+        break;
     default:
-      break;
-  }
+    break;
+}
 };
