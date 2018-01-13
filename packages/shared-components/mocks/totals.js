cd44c7516c014dd547225d9276275337f4da8885
Federico Weber <federicoweber@gmail.com>
2018-01-12 17:09:51 -0800

    add story to ComparisonChart

---
+++ packages/shared-components/mocks/totals.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    color: '#fda3f3',
+    diff: 25,
+    label: 'Impression',
+    value: 150,
+    previousValue: 100,
+  },
+  {
+    color: '#fda3f3',
+    diff: -55,
+    label: 'Click',
+    value: 1010,
+    previousValue: 100,
+  },
+  {
+    color: '#fda3f3',
+    diff: -39,
+    label: 'Engagement',
+    value: 901,
+    previousValue: 100,
+  },
+  {
+    color: '#fda3f3',
+    diff: -39,
+    label: 'Posts',
+    value: 100,
+    previousValue: 100,
+  },
+];
