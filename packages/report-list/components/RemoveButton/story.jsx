53527bb8849d63aed561e6c6cfe790d0ea99acd5
James Morris <me@jamesmorr.is>
2018-01-15 17:29:42 +0000

    Adding test coverage for new remove button

---
+++ packages/report-list/components/RemoveButton/story.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { checkA11y } from 'storybook-addon-a11y';
+import { action } from '@storybook/addon-actions';
+import RemoveButton from './index';
+
+const report = {
+  _id: '1293125asda',
+};
+
+storiesOf('RemoveButton')
+  .addDecorator(checkA11y)
+  .add('renders the remove button', () => (
+    <RemoveButton
+      {...report}
+      removeReport={action('remove report')}
+    />
+  ));
+
