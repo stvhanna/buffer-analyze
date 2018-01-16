69f193c15a4f6ef7c8005a9c8a8e382c9aa435f5
Mike San Román <mike@msanroman.io>
2018-01-16 12:01:03 +0100

    Test properly onClick

--- packages/nav-sidebar/index.test.jsx
+++ packages/nav-sidebar/index.test.jsx
@@ -6,6 +6,8 @@ import NavSidebar, {
   reducer,
 } from './index';
 import Insights from './components/Insights';
+import Item from './components/Item';
+import { Link } from '@bufferapp/components';
 
 configure({ adapter: new Adapter() });
 const storeFake = state => ({
@@ -19,20 +21,18 @@ describe('NavSidebar', () => {
   it('should render with links to Insights', () => {
     const store = storeFake({
       navSidebar: {
-        profiles: [
-          {
-            id: '2',
-            service: 'instagram',
-          },
-          {
-            id: '4',
-            service: 'twitter',
-          },
-          {
-            id: '5',
-            service: 'facebook',
-          },
-        ],
+        instagramProfile: {
+          id: '2',
+          service: 'instagram',
+        },
+        twitterProfile: {
+          id: '4',
+          service: 'twitter',
+        },
+        facebookProfile: {
+          id: '5',
+          service: 'facebook',
+        },
       },
     });
     const wrapper = mount(
@@ -43,6 +43,63 @@ describe('NavSidebar', () => {
     expect(wrapper.find(NavSidebar).length)
       .toBe(1);
     expect(wrapper.find(Insights)).toHaveLength(1);
+  });
+
+  describe('clicking on items', () => {
+    let store;
+
+    beforeEach(() => {
+      store = storeFake({
+        navSidebar: {
+          instagramProfile: {
+            id: '2',
+            service: 'instagram',
+          },
+          twitterProfile: {
+            id: '4',
+            service: 'twitter',
+          },
+          facebookProfile: {
+            id: '5',
+            service: 'facebook',
+          },
+        },
+      });
+    });
+
+    it('should dispatch CALL_HISTORY_METHOD and SELECT_PROFILE_SERVICE', () => {
+      const wrapper = mount(
+        <Provider store={store}>
+          <NavSidebar route="/" />
+        </Provider>,
+      );
+      const link = wrapper
+        .find(Insights)
+        .find(Link)
+        .get(0);
+      link.props.onClick({ preventDefault: () => {} });
+      expect(store.dispatch).toHaveBeenCalledTimes(2);
+      expect(store.dispatch.mock.calls[0][0]).toMatchObject({
+        type: '@@router/CALL_HISTORY_METHOD',
+      });
+      expect(store.dispatch.mock.calls[1][0]).toMatchObject({
+        type: 'SELECT_PROFILE_SERVICE',
+      });
+    });
+
+    it('should not dispatch anything if item is active already', () => {
+      const wrapper = mount(
+        <Provider store={store}>
+          <NavSidebar route="/insights/facebook/5/overview" />
+        </Provider>,
+      );
+      const link = wrapper
+        .find(Insights)
+        .find(Link)
+        .get(0);
+      link.props.onClick({ preventDefault: () => {} });
+      expect(store.dispatch).toHaveBeenCalledTimes(0);
+    });
   });
 
   it('should render without links to Insights if there are no profiles', () => {
@@ -59,26 +116,6 @@ describe('NavSidebar', () => {
     expect(wrapper.find(NavSidebar).length)
       .toBe(1);
     expect(wrapper.find(Insights).children()).toHaveLength(0);
-  });
-
-  it('it should export onClick', () => {
-    const store = storeFake({
-      navSidebar: {
-        profiles: [],
-      },
-    });
-    const component = shallow(
-      <NavSidebar store={store} route="/" />,
-    );
-    expect(component.props().onClick).toBeDefined();
-    component.props().onClick();
-    expect(store.dispatch).toHaveBeenCalledTimes(2);
-    expect(store.dispatch.mock.calls[0][0]).toMatchObject({
-      type: '@@router/CALL_HISTORY_METHOD',
-    });
-    expect(store.dispatch.mock.calls[1][0]).toMatchObject({
-      type: 'SELECT_PROFILE_SERVICE',
-    });
   });
 
   it('should export reducer', () => {
