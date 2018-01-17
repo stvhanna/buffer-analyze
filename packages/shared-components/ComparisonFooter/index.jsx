956a5078bc31109df577666e75b2ea033f4b78d0
Federico Weber <federicoweber@gmail.com>
2018-01-16 16:04:43 -0800

    use profile to generate  ComparisonFooter

--- packages/shared-components/ComparisonFooter/index.jsx
+++ packages/shared-components/ComparisonFooter/index.jsx
@@ -29,28 +29,59 @@ const ProfileUsernameWrapper = styled.div`
   margin-right: 10px;
 `;
 
-const ComparisonFooter = ({ profileTotals }) => (
+const ProfileCell = ({ profileTotal, profile }) => (
+  <GridItem
+    metric={{
+      label: profileTotal.metric.label,
+      value: profileTotal.currentPeriodTotal,
+      diff: profileTotal.currentPeriodDiff,
+    }}
+    customLabel={
+      <ProfileAvatarWrapper>
+        <ProfileUsernameWrapper>
+          <Text size="small">
+            {profile.username}
+          </Text>
+        </ProfileUsernameWrapper>
+        <MetricIcon key={profile.profileId} metric={profileTotal.metric} />
+      </ProfileAvatarWrapper>
+    }
+  />
+);
+
+ProfileCell.propTypes = {
+  profileTotal: PropTypes.shape({
+    metric: PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      color: PropTypes.string.isRequired,
+    }),
+    currentPeriodTotal: PropTypes.number.isRequired,
+    currentPeriodDiff: PropTypes.number.isRequired,
+    profileId: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    service: PropTypes.string,
+  }).isRequired,
+  profile: PropTypes.shape({
+    profileId: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    avatarUrl: PropTypes.string.isRequired,
+    service: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+function renderGridItem(profileTotal) {
+  const profile = this.profiles.find(p => p.id === profileTotal.profileId);
+  return (<ProfileCell
+    key={profileTotal.profileId}
+    profileTotal={profileTotal}
+    profile={profile}
+  />);
+}
+
+const ComparisonFooter = ({ profileTotals, profiles }) => (
   <Wrapper>
     <Grid>
-      {profileTotals.map(total =>
-        <GridItem
-          key={total.profileId}
-          metric={{
-            label: total.metric.label,
-            value: total.currentPeriodTotal,
-            diff: total.currentPeriodDiff,
-          }}
-          customLabel={
-            <ProfileAvatarWrapper>
-              <ProfileUsernameWrapper>
-                <Text size="small">
-                  {total.username}
-                </Text>
-              </ProfileUsernameWrapper>
-              <MetricIcon key={total.profileId} metric={total.metric} />
-            </ProfileAvatarWrapper>
-          }
-        />)}
+      {profileTotals.map(renderGridItem, { profiles })}
     </Grid>
   </Wrapper>
 );
@@ -70,6 +101,12 @@ ComparisonFooter.propTypes = {
     profileId: PropTypes.string.isRequired,
     username: PropTypes.string.isRequired,
     service: PropTypes.string,
+  })).isRequired,
+  profiles: PropTypes.arrayOf(PropTypes.shape({
+    profileId: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    avatarUrl: PropTypes.string.isRequired,
+    service: PropTypes.string.isRequired,
   })).isRequired,
 };
 
