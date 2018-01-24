108b9d9c35a9f9c59e9a71ccb86eafac80a352b7
tom <tom@buffer.com>
2018-01-24 12:17:51 -0500

    halfway done using styled components...

--- packages/nav-sidebar/components/FeedbackLink.jsx
+++ packages/nav-sidebar/components/FeedbackLink.jsx
@@ -4,26 +4,26 @@ import {
   Link
 } from '@bufferapp/components';
 
-const feedbackSpanStyle = {
-	display: 'block',
-	padding: '0.75rem 0.5rem',
-	margin: '0px 0.5rem',
-	borderRadius: '4px'
-};
+ const FeedbackSpan = styled.span`
+    display: block;
+	padding: 0.75rem 0.5rem;
+	margin: 0px 0.5rem;
+	borderRadius: 4px;
+ `;
 
-const feedbackLinkStyle = {
-	fontFamily: '"Roboto", sans-serif',
-	fontSize: '1rem',
-	fontWeight: '400',
-	color: 'rgb(89, 98, 106)',
-	textDecoration: 'none',
-};
+ const FeedbackLinkSpan = styled.span`
+     fontFamily: "Roboto", sans-serif;
+	fontSize: 1rem;
+	fontWeight: 400;
+	color: rgb(89, 98, 106);
+	textDecoration: none;
+ `;
 
 const FeedbackLink = ({ children, email }) => (
 	<Link unstyled href={'mailto:' + email + '?subject=Analyze feedback'}>
-		<span style={feedbackSpanStyle}>
-	    	<span style={feedbackLinkStyle}>{children}</span>
-    	</span>
+		<FeedbackSpan>
+	    	<FeedbackLinkSpan>{children}</FeedbackLinkSpan>
+    	</FeedbackSpan>
     </Link>
 );
 
