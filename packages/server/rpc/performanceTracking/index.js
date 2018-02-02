440a0d53da7f1c540564183ad8868f75abb6a070
Federico Weber <federicoweber@gmail.com>
2018-02-01 17:26:12 -0800

    add data validation

--- packages/server/rpc/performanceTracking/index.js
+++ packages/server/rpc/performanceTracking/index.js
@@ -1,4 +1,4 @@
-const { method } = require('@bufferapp/micro-rpc');
+const { method, createError } = require('@bufferapp/micro-rpc');
 const StatsD = require('node-dogstatsd').StatsD;
 
 const dogstatsd = new StatsD('dd-agent.default');
@@ -8,6 +8,8 @@ module.exports = method(
   'track performance',
   ({ data }) => {
     if (!data.tags) data.tags = [];
+    if (!data.name) throw createError({ message: 'can\'t save a measure without a valid data.name' });
+    if (!data.duration) throw createError({ message: 'can\'t save a measure without a valid data.duration' });
     data.tags.push('app:analyze');
     data.name = `buffer.perf.${data.name}`;
 
