589db97372a7f0d78d368a662b98632f02fe98e3
Mike San Román <mike@msanroman.io>
2018-02-06 19:20:49 +0100

    Keep file as "inline", set pdf extension on both headers

--- packages/server/index.js
+++ packages/server/index.js
@@ -137,7 +137,7 @@ app.get('/report_to_pdf', (req, res) => {
     region: 'us-east-1',
   });
   lambda.invoke(params, (err, data) => {
-    res.header('Content-disposition', `inline; filename=${req.query.name}`);
+    res.header('Content-disposition', `inline; filename=${req.query.name}.pdf`);
     if (err) {
       res.send(err);
     } else {
@@ -147,7 +147,7 @@ app.get('/report_to_pdf', (req, res) => {
       } else {
         const pdf = Buffer.from(payload.contents, 'base64');
         res.type('application/pdf');
-        res.header('Content-disposition', `attachment; filename=${req.query.name}.pdf`);
+        res.header('Content-disposition', `inline; filename=${req.query.name}.pdf`);
         res.end(pdf, 'binary');
       }
     }
