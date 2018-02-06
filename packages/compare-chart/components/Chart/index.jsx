f8314644b1c4e135b10a56fdb43ca3f7a2bfce9c
James Morris <me@jamesmorr.is>
2018-02-06 17:10:57 +0000

    Little adjustments to gradient for visual calmness

--- packages/compare-chart/components/Chart/index.jsx
+++ packages/compare-chart/components/Chart/index.jsx
@@ -71,8 +71,8 @@ function prepareSeries(
     fillColor: {
       linearGradient: [0, 0, 0, 300],
       stops: [
-        [0, fadeColor(color, 0.3)],
-        [1, fadeColor(color, 0)],
+        [0, fadeColor(color, 0.6)],
+        [1, fadeColor(color, 0.1)],
       ],
     },
     lineColor: color,
@@ -87,7 +87,7 @@ function prepareSeries(
       linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
       stops: [
         [0, fadeColor(color, 0.6)],
-        [1, fadeColor(color, 0)],
+        [1, fadeColor(color, 0.2)],
       ],
     }];
     seriesConfig.borderColor = color;
