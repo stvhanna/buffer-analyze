2215669bbf3bee5743487cf2972e9fe7f7ff6e84
Mike San Román <mike@msanroman.io>
2018-02-08 18:29:29 +0100

    New dashboard link

--- packages/web/components/DefaultPage/index.jsx
+++ packages/web/components/DefaultPage/index.jsx
@@ -30,54 +30,16 @@ const navigate = (event, dispatch, url) => {
   dispatch(push(url));
 };
 
-const DefaultPage = ({ location, dispatch, facebookProfile, twitterProfile, instagramProfile }) => {
-  let facebookLink = 'Facebook Pages';
-  let twitterLink = 'Twitter Profiles';
-  let instagramLink = 'Instagram Profiles';
-
-  const comparisonsLink = (<Link
-    href="/comparisons"
-    unstyled
-    onClick={e => navigate(e, dispatch, '/comparisons')}
-  >compare profiles</Link>);
+const DefaultPage = ({ location, dispatch }) => {
   const reportsLink = (<Link
     href="/reports"
     unstyled
     onClick={e => navigate(e, dispatch, '/reports')}
   >your reports</Link>);
 
-  if (facebookProfile) {
-    const facebookInsightsURL = `/insights/facebook/${facebookProfile.id}/overview`;
-    facebookLink = (<Link
-      href={facebookInsightsURL}
-      unstyled
-      onClick={e => navigate(e, dispatch, facebookInsightsURL)}
-    >Facebook Pages</Link>);
-  }
-
-  if (twitterProfile) {
-    const twitterInsightsURL = `/insights/twitter/${twitterProfile.id}/overview`;
-    twitterLink = (<Link
-      href={twitterInsightsURL}
-      unstyled
-      onClick={e => navigate(e, dispatch, twitterInsightsURL)}
-    >Twitter Profiles</Link>);
-  }
-
-  if (instagramProfile) {
-    const instagramInsightsURL = `/insights/instagram/${instagramProfile.id}/overview`;
-    instagramLink = (<Link
-      href={instagramInsightsURL}
-      unstyled
-      onClick={e => navigate(e, dispatch, instagramInsightsURL)}
-    >Instagram Profiles</Link>);
-  }
-
   const welcomeText = (
     <span>
-      Get started by viewing Insights for {facebookLink}, {twitterLink} or {instagramLink}.
-      <br />
-      You can also view {reportsLink}.
+      Get started by viewing an <Link href="/overview" unstyled onClick={e => navigate(e, dispatch, '/overview')}>overview of your performance</Link> or gaining some insights around <Link href="/posts" unstyled onClick={e => navigate(e, dispatch, '/posts')}>your posts</Link>. You can also view {reportsLink}.
     </span>
   );
 
@@ -99,34 +61,11 @@ const DefaultPage = ({ location, dispatch, facebookProfile, twitterProfile, inst
   );
 };
 
-DefaultPage.defaultProps = {
-  instagramProfile: null,
-  twitterProfile: null,
-  facebookProfile: null,
-};
-
 DefaultPage.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired,
   }).isRequired,
-  instagramProfile: PropTypes.shape({
-    service: PropTypes.String,
-  }),
-  twitterProfile: PropTypes.shape({
-    service: PropTypes.String,
-  }),
-  facebookProfile: PropTypes.shape({
-    service: PropTypes.String,
-  }),
   dispatch: PropTypes.func.isRequired,
 };
 
-function mapStateToProps (state) {
-  return {
-    facebookProfile: state.navSidebar.facebookProfile,
-    twitterProfile: state.navSidebar.twitterProfile,
-    instagramProfile: state.navSidebar.instagramProfile,
-  };
-}
-
-export default connect(mapStateToProps)(DefaultPage);
+export default connect()(DefaultPage);
