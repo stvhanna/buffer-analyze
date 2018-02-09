a69ce6e718c8dfd494c3b7345163f4a2e0b13f8d
James Morris <me@jamesmorr.is>
2018-02-09 19:08:44 +0000

    Fix mock data for eslint and update snapshots

--- packages/shared-components/GridItem/mock/daily7days.js
+++ packages/shared-components/GridItem/mock/daily7days.js
@@ -1,142 +1,142 @@
 export default [
   {
-    "day":"1517443200000",
-    "metrics":[
+    day: '1517443200000',
+    metrics: [
       {
-        "diff":-26,
-        "label":"Engagement average",
-        "value":230
+        diff: -26,
+        label: 'Engagement average',
+        value: 230,
       },
       {
-        "diff":-25,
-        "label":"Impression average",
-        "value":35620
+        diff: -25,
+        label: 'Impression average',
+        value: 35620,
       },
       {
-        "diff":-17,
-        "label":"Click average",
-        "value":290
-      }
-    ]
+        diff: -17,
+        label: 'Click average',
+        value: 290,
+      },
+    ],
   },
   {
-    "day":"1517529600000",
-    "metrics":[
+    day: '1517529600000',
+    metrics: [
       {
-        "diff":-21,
-        "label":"Engagement average",
-        "value":161
+        diff: -21,
+        label: 'Engagement average',
+        value: 161,
       },
       {
-        "diff":-16,
-        "label":"Impression average",
-        "value":28260
+        diff: -16,
+        label: 'Impression average',
+        value: 28260,
       },
       {
-        "diff":8,
-        "label":"Click average",
-        "value":308
-      }
-    ]
+        diff: 8,
+        label: 'Click average',
+        value: 308,
+      },
+    ],
   },
   {
-    "day":"1517616000000",
-    "metrics":[
+    day: '1517616000000',
+    metrics: [
       {
-        "diff":-52,
-        "label":"Engagement average",
-        "value":157
+        diff: -52,
+        label: 'Engagement average',
+        value: 157,
       },
       {
-        "diff":-52,
-        "label":"Impression average",
-        "value":28314
+        diff: -52,
+        label: 'Impression average',
+        value: 28314,
       },
       {
-        "diff":10,
-        "label":"Click average",
-        "value":324
-      }
-    ]
+        diff: 10,
+        label: 'Click average',
+        value: 324,
+      },
+    ],
   },
   {
-    "day":"1517702400000",
-    "metrics":[
+    day: '1517702400000',
+    metrics: [
       {
-        "diff":-41,
-        "label":"Engagement average",
-        "value":167
+        diff: -41,
+        label: 'Engagement average',
+        value: 167,
       },
       {
-        "diff":-47,
-        "label":"Impression average",
-        "value":28595
+        diff: -47,
+        label: 'Impression average',
+        value: 28595,
       },
       {
-        "diff":2,
-        "label":"Click average",
-        "value":350
-      }
-    ]
+        diff: 2,
+        label: 'Click average',
+        value: 350,
+      },
+    ],
   },
   {
-    "day":"1517788800000",
-    "metrics":[
+    day: '1517788800000',
+    metrics: [
       {
-        "diff":-61,
-        "label":"Engagement average",
-        "value":138
+        diff: -61,
+        label: 'Engagement average',
+        value: 138,
       },
       {
-        "diff":-46,
-        "label":"Impression average",
-        "value":25506
+        diff: -46,
+        label: 'Impression average',
+        value: 25506,
       },
       {
-        "diff":33,
-        "label":"Click average",
-        "value":380
-      }
-    ]
+        diff: 33,
+        label: 'Click average',
+        value: 380,
+      },
+    ],
   },
   {
-    "day":"1517875200000",
-    "metrics":[
+    day: '1517875200000',
+    metrics: [
       {
-        "diff":-1,
-        "label":"Engagement average",
-        "value":154
+        diff: -1,
+        label: 'Engagement average',
+        value: 154,
       },
       {
-        "diff":-8,
-        "label":"Impression average",
-        "value":23968
+        diff: -8,
+        label: 'Impression average',
+        value: 23968,
       },
       {
-        "diff":17,
-        "label":"Click average",
-        "value":309
-      }
-    ]
+        diff: 17,
+        label: 'Click average',
+        value: 309,
+      },
+    ],
   },
   {
-    "day":"1517961600000",
-    "metrics":[
+    day: '1517961600000',
+    metrics: [
       {
-        "diff":-57,
-        "label":"Engagement average",
-        "value":133
+        diff: -57,
+        label: 'Engagement average',
+        value: 133,
       },
       {
-        "diff":-53,
-        "label":"Impression average",
-        "value":23567
+        diff: -53,
+        label: 'Impression average',
+        value: 23567,
       },
       {
-        "diff":-13,
-        "label":"Click average",
-        "value":344
-      }
-    ]
-  }
+        diff: -13,
+        label: 'Click average',
+        value: 344,
+      },
+    ],
+  },
 ];
