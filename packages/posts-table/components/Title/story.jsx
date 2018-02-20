b0f523f5de2ea8389d69dbb0ed5f43cc58377783
James Morris <me@jamesmorr.is>
2018-02-20 12:29:23 +0000

    Add stories for all chart titles

---
+++ packages/posts-table/components/Title/story.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { checkA11y } from 'storybook-addon-a11y';
+import Title from './index';
+
+storiesOf('Title')
+  .addDecorator(checkA11y)
+  .add('should render title as if for app', () => (
+    <Title profileService="facebook" />
+  ))
+  .add('should render title as if for report', () => (
+    <Title profileService="facebook" forReport />
+  ))
+  .add('should render title for Twitter is if for app', () => (
+    <Title profileService="twitter" />
+  ))
+  .add('should render title for Twitter is if for report', () => (
+    <Title profileService="twitter" forReport />
+  ));
