b5f69af178977512cd3b24c9a54bfc2b32052f9c
Mike San Román <mike@msanroman.io>
2018-03-26 11:40:31 +0200

    Add more meaningful titles for Contextual charts con reports

--- packages/contextual-compare/components/Title/index.jsx
+++ packages/contextual-compare/components/Title/index.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ChartTitle } from '@bufferapp/analyze-shared-components';
 
-const Title = ({ forReport }) => (
+const Title = ({ forReport, state, presets }) =>
   <ChartTitle forReport={forReport}>
-    Engagement and audience
-  </ChartTitle>
-);
+    { (forReport && state.mode === 0) ? presets[state.selectedPreset].label : 'Engagement and audience' }
+  </ChartTitle>;
 
 Title.propTypes = {
   forReport: PropTypes.bool,
+  state: PropTypes.shape({
+    selectedPreset: PropTypes.number,
+  }),
+  presets: PropTypes.arrayOf({
+    label: PropTypes.string,
+  }),
 };
 
 Title.defaultProps = {
   forReport: false,
+  state: {},
+  presets: [],
 };
 
 export default Title;
---
+++ packages/contextual-compare/components/Title/story.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { checkA11y } from 'storybook-addon-a11y';
+import Title from './index';
+
+const presets = [{
+  label: 'When is it most effective to post?',
+}];
+
+storiesOf('Title')
+  .addDecorator(checkA11y)
+  .add('[TESTED] should render title as if for app', () => (
+    <Title />
+  ))
+  .add('[TESTED] should render title as if for report', () => (
+    <Title forReport state={{ selectedPreset: 0 }} presets={presets} />
+  ))
+  .add('[TESTED] should render regular title even if for report if no preset was selected', () => (
+    <Title forReport state={{ selectedPreset: 0 }} presets={presets} />
+  ));
+
