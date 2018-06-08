f25a0d3f86e6d75037cb3d861d54851154b96b8e
James Morris <me@jamesmorr.is>
2018-06-08 13:04:32 +0100

    Move chart card to styled components for styles

--- packages/shared-components/ChartCard/index.jsx
+++ packages/shared-components/ChartCard/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { white } from '@bufferapp/components/style/color';
+import styled from 'styled-components';
 
-const style = {
-  background: `${white}`,
-  border: '1px solid #E2E8ED',
-  boxShadow: '0px 0px 10px rgba(48, 71, 89, 0.05)',
-  borderRadius: '5px',
-  margin: '0 0 2rem',
-};
+const Container = styled.div`
+  background: #FFFFFF;
+  border: 1px solid #E2E8ED;
+  box-shadow: 0px 0px 10px rgba(48, 71, 89, 0.05);
+  border-radius: 5px;
+  margin: 0 0 2rem;
+`;
 
 const ChartCard = ({ children }) =>
-  <div style={style}>{children}</div>;
+  <Container>{children}</Container>;
 
 ChartCard.propTypes = {
   children: PropTypes.node.isRequired,
