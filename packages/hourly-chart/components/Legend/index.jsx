c958d9f7a1f247e00e008d7fd821e0e87a507999
James Morris <me@jamesmorr.is>
2018-06-11 14:03:09 +0100

    Move hourly charts legends over to styled components

--- packages/hourly-chart/components/Legend/index.jsx
+++ packages/hourly-chart/components/Legend/index.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import {
   Text,
 } from '@bufferapp/components';
 import ColorIcon from '../ColorIcon';
 
-const legendItem = {
-  display: 'inline-block',
-  padding: '.8rem',
-  margin: '0 .8rem',
-};
+const LegendList = styled.ul`
+  padding: 0.75rem 1.5rem;
+  margin: 0 0 1rem;
+  text-align: center;
+`;
 
-const legendList = {
-  padding: '0 1.5rem',
-  marginBottom: '1.5rem',
-  textAlign: 'center',
-};
+const LegendItem = styled.li`
+  display: inline-block;
+  padding: 0.8rem;
+  margin: 0 .8rem;
+`;
 
 const Legend = ({ metric, secondaryMetric }) =>
-  <ul style={legendList}>
-    <li style={legendItem}>
+  <LegendList>
+    <LegendItem>
       <ColorIcon circle />
       <Text size="small">Tweets</Text>
-    </li>
-    <li style={legendItem}>
+    </LegendItem>
+    <LegendItem>
       <ColorIcon metric={metric.label} circle />
       <Text size="small">{metric.label}</Text>
-    </li>
-    { secondaryMetric && <li style={legendItem}>
+    </LegendItem>
+    { secondaryMetric && <LegendItem>
       <ColorIcon metric={secondaryMetric.label} circle />
       <Text size="small">{secondaryMetric.label}</Text>
-    </li> }
-  </ul>;
+    </LegendItem> }
+  </LegendList>;
 
 Legend.defaultProps = {
   secondaryMetric: null,
