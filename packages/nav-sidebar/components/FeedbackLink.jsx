a3108c2e1ab02af17650b28807ec72d058fa64de
Mike San Román <mike@msanroman.io>
2018-08-07 13:21:05 +0200

    Tweak "Send Feedback" link

--- packages/nav-sidebar/components/FeedbackLink.jsx
+++ packages/nav-sidebar/components/FeedbackLink.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { curiousBlue } from '@bufferapp/components/style/color';
+import { MessageIcon } from '@bufferapp/components';
 
 import {
   Text,
@@ -9,7 +10,8 @@ import {
 } from '@bufferapp/components';
 
 const InnerLink = styled.span`
-  display: block;
+  display: flex;
+  align-items: flex-start;
   padding: 0.75rem 0.5rem;
   margin: 0 0.5rem;
   border-radius: 4px;
@@ -19,10 +21,15 @@ const InnerLink = styled.span`
   }
 `;
 
+const TextWrapper = styled.span`
+  margin-left: .5rem;
+  display: inline-flex;
+`;
+
 const FeedbackLink = ({ children, email }) => (
   <Link unstyled href={`mailto:${email}?subject=Analyze feedback`}>
     <InnerLink>
-      <Text color="shuttleGray">{children}</Text>
+      <MessageIcon /> <TextWrapper><Text size="small" color="shuttleGray">{children}</Text></TextWrapper>
     </InnerLink>
   </Link>
 );
