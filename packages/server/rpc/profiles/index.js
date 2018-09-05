051c907f44b35cba6d3a7b8775cae306daf8da2d
Federico Weber <federicoweber@gmail.com>
2018-09-04 17:14:27 -0700

    sort profiles by username

--- packages/server/rpc/profiles/index.js
+++ packages/server/rpc/profiles/index.js
@@ -2,6 +2,15 @@ const { method } = require('@bufferapp/micro-rpc');
 const rp = require('request-promise');
 const profileParser = require('../utils/profileParser');
 
+function getComparableUsername(username) {
+  return username.replace('@', '').toUpperCase();
+}
+
+function sortProfilesByUsername(profiles) {
+  return profiles.sort((a, b) =>
+    getComparableUsername(a.username).localeCompare(getComparableUsername(b.username)));
+}
+
 module.exports = method(
   'profiles',
   'fetch profiles',
@@ -9,11 +18,11 @@ module.exports = method(
     rp({
       uri: `${process.env.API_ADDR}/1/analyze/users/${id}/get_social_accounts.json`,
       method: 'GET',
-      strictSSL: !(process.env.NODE_ENV === 'development'),
+      strictSSL: !(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'),
       qs: {
         access_token: session.analyze.accessToken,
       },
     })
       .then(result => JSON.parse(result))
-      .then(({ profiles }) => profiles.map(profileParser)),
+      .then(({ profiles }) => sortProfilesByUsername(profiles.map(profileParser))),
 );
